6150bfd9aac82258c454d1abf3d8cdd500a8fc2a
shibnath96 <shibnath96@gmail.com>
2020-06-07 22:32:43 +0530

    Basic setup

--- index.js
+++ index.js
@@ -13,10 +13,21 @@ app.use('/assets', express.static(__dirname + '/views/assets/'));
 app.use('/bootstrap/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
 app.use('/bootstrap/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
 app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
+app.use('/js', express.static(__dirname + '/views/js'));
+app.use('/css', express.static(__dirname + '/views/css'));
+app.use('/pages', express.static(__dirname + '/views/pages'));
 
 app.get('/', (req, res) => {
-	res.render('index')
+	res.render('index');
 });
+
+app.get('/add_new', (req, res) => {
+	res.render('newEntry');
+});
+
+app.get('/history', (req, res) => {
+	res.render('history');
+})
 
 app.listen(3000, () => {
 	console.log('Application is running- localhost:3000');
---
+++ views/js/index.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+    var navContent = $('#bd-docs-nav');
+    navContent.show();
+})
